7a999cccf74b7d9369a73f6265ac81de6c16f527
naitsircnat <cristiangerardchen@gmail.com>
2025-01-03 08:46:05 +0000

    Allowed for red and green box through buttons

--- 03-events-and-state/src/ColoredBox.jsx
+++ 03-events-and-state/src/ColoredBox.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function ColoredBox() {
-    return (
-        <>
-            <div style={{
-                margin:"10px",
-                backgroundColor: "white",
-                border: "2px solid black",
-                height: "50px",
-                width: "50px"
-            }} />
-            <button>Red</button><button>Green</button>
-        </>
-    )
+  const [colour, setColour] = useState("red");
+
+  const red = () => {
+    setColour("red");
+  };
+
+  const green = () => {
+    setColour("green");
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          margin: "10px",
+          backgroundColor: colour,
+
+          border: "2px solid black",
+          height: "50px",
+          width: "50px",
+        }}
+      />
+      <button onClick={red}>Red</button>
+      <button onClick={green}>Green</button>
+    </>
+  );
 }
